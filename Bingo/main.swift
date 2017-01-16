0445cfd0be947d66d1c631c4f9e2e941c8ecebcb
Student <liam@macbook-pro-byrne.local>
2017-01-16 15:50:10 -0500

    Created the initial variables and arrays, and started to generate random numbers and take user input

--- Bingo/main.swift
+++ Bingo/main.swift
@@ -8,5 +8,19 @@
 
 import Foundation
 
-print("Hello, World!")
+// Declare an array of integers to store the selected numbers
+var select : [Int]
 
+// Declare a variable to check to make sure input is an integer
+var number = 0
+
+// Declare two variables to store die from rolled dice
+var firstRoll = arc4random_uniform(5)
+var secondRoll = arc4random_uniform(5)
+
+// Declare a variable to temperarily store data from command line
+if let input = Int(readLine(strippingNewline: true)!) {
+    number = input
+}
+
+print(number)

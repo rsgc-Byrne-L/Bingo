7d91d5d87985d61636ec1f4d30241d20a1007732
Student <liam@macbook-pro-byrne.local>
2017-01-16 15:49:24 -0500

    Initial Commit

---
+++ Bingo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Bingo
+//
+//  Created by Liam Byrne on 2017-01-16.
+//  Copyright © 2017 Liam Byrne. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

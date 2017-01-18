a13e3caf3c2315e7e476c034de9e72684354e516
Student <liam@macbook-pro-byrne.local>
2017-01-18 12:51:22 -0500

    Added the check to see if your selected number was rolled, and the check if your number was rolled again

--- Bingo/main.swift
+++ Bingo/main.swift
@@ -8,30 +8,58 @@
 
 import Foundation
 
-// Declare an array of integers to store the selected numbers
-var select = [0, 0, 0, 0, 0]
+// Declare an array of integers to store the user's queue of numbers
+var queue = [0, 0, 0, 0, 0]
+
+// Declare an array of bools to store the selected number data
+var selected = [false, false, false, false, false]
 
 // Declare a variable to check to make sure input is an integer
 var number = 0
 
-// Declare a variable to count up for loo[s
+// Declare a variable to count up for loops
 var count = 0
-
-// Declare two variables to store data from rolled dice
-var roll = arc4random_uniform(10)
+var count2 = 0
+// Declare a variable to store data from rolled dice
+var roll = Int(arc4random_uniform(10))
 
 while count < 5 {
     
     // Declare a variable to temperarily store data from command line
-    print("Type a number to add to your queue - (Number \(count))")
+    print("Type a number to add to your queue - (Number \(count+1))")
     if let input = Int(readLine(strippingNewline: true)!) {
         number = input
-        select[count] = number
+        if number >= 2 && number <= 12 {
+            queue[count] = number
+            count += 1
+        }
     }
     
-    count += 1
-    
-    print(select)
+    print(queue)
     
 }
 
+count = 0
+
+while count <= 4 {
+    roll = Int(arc4random_uniform(10) + 2)
+    
+    while count2 < 4 {
+    if roll == queue[count2] {
+        if selected[count2] == true {
+            selected[count2] = false
+        } else {
+        selected[count2] = true
+        }
+        count2 += 1
+    } else {
+        count2 += 1
+        }
+    }
+    count += 1
+    
+    count2 = 0
+    
+    print(roll)
+    print(selected)
+}

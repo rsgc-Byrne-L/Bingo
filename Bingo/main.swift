e34845f085551bec03291bc6e8c9d603e0023009
Student <liam@macbook-pro-byrne.local>
2017-01-18 00:38:33 -0500

    User can now input selections to place in the array

--- Bingo/main.swift
+++ Bingo/main.swift
@@ -9,18 +9,29 @@
 import Foundation
 
 // Declare an array of integers to store the selected numbers
-var select : [Int]
+var select = [0, 0, 0, 0, 0]
 
 // Declare a variable to check to make sure input is an integer
 var number = 0
 
-// Declare two variables to store die from rolled dice
-var firstRoll = arc4random_uniform(5)
-var secondRoll = arc4random_uniform(5)
+// Declare a variable to count up for loo[s
+var count = 0
 
-// Declare a variable to temperarily store data from command line
-if let input = Int(readLine(strippingNewline: true)!) {
-    number = input
+// Declare two variables to store data from rolled dice
+var roll = arc4random_uniform(10)
+
+while count < 5 {
+    
+    // Declare a variable to temperarily store data from command line
+    print("Type a number to add to your queue - (Number \(count))")
+    if let input = Int(readLine(strippingNewline: true)!) {
+        number = input
+        select[count] = number
+    }
+    
+    count += 1
+    
+    print(select)
+    
 }
 
-print(number)

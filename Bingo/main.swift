4de008e99a59f6b5b8fc9e3ad0c7c6275693d34c
Liam Byrne <liammbyrnee@gmail.com>
2017-01-26 09:28:33 -0500

    Added test mode, working to add total score after x number of rounds

--- Bingo/main.swift
+++ Bingo/main.swift
@@ -14,10 +14,13 @@ var queue = [0, 0, 0, 0, 0]
 // Declare an array of bools to store the selected number data
 var selected = [false, false, false, false, false]
 
+var scores = [Int]()
+
 // Declare variables to count and calculate score
 var score = 0
 var total = 0
 var temp = 0
+var hold = 0
 var choice = 0
 
 // Declare a variable to count up for loops
@@ -45,59 +48,13 @@ while temp == 0 {
 
 // To play
 if choice == 0 {
-temp = 0
-
-// Section to take user input and put it into array
-while temp <= 4 {
+    temp = 0
     
-    // Declare a variable to temperarily store data from command line
-    print("Type a number to add to your queue - (Number \(temp+1))")
-    if let input = Int(readLine(strippingNewline: true)!) {
-        if input >= 2 && input <= 12 {
-            queue[temp] = input
-            total = total + input
-            temp += 1
-        }
-    }
-}
-
-temp = 0
-
-for (index, value) in queue.enumerated() {
-    roll = Int(arc4random_uniform(10) + 2)
-    
-    for (index, value) in queue.enumerated() {
-        if roll == queue[index] {
-            if selected[index] == true {
-                selected[index] = false
-            } else {
-                selected[index] = true
-            }
-        }
-    }
-    
-    print(roll)
-    print(selected)
-}
-
-
-for (index, value) in queue.enumerated() {
-    if selected[index] == true {
-        temp = queue[index]
-        total = total - temp
-    }
-}
-    
-temp = 0
-
-print("Your score is \(total)")
-// To test
-} else if choice == 1 {
     // Section to take user input and put it into array
     while temp <= 4 {
         
         // Declare a variable to temperarily store data from command line
-        print("Type a number to add to your queue for testing - (Number \(temp+1))")
+        print("Type a number to add to your queue - (Number \(temp+1))")
         if let input = Int(readLine(strippingNewline: true)!) {
             if input >= 2 && input <= 12 {
                 queue[temp] = input
@@ -106,4 +63,86 @@ print("Your score is \(total)")
             }
         }
     }
+    
+    temp = 0
+    
+    for (index, value) in queue.enumerated() {
+        roll = Int(arc4random_uniform(10) + 2)
+        
+        for (index, value) in queue.enumerated() {
+            if roll == queue[index] {
+                if selected[index] == true {
+                    selected[index] = false
+                } else {
+                    selected[index] = true
+                }
+            }
+        }
+    }
+    
+    
+    for (index, value) in queue.enumerated() {
+        if selected[index] == true {
+            temp = queue[index]
+            total = total - temp
+        }
+    }
+    
+    temp = 0
+    
+    print("Your score is \(total)")
+    // To test
+} else if choice == 1 {
+    temp = 0
+    while temp == 0 {
+        print("How many times would you like to test? (1 - 1,000,000)")
+        if let testNumber = Int(readLine(strippingNewline: true)!) {
+            if testNumber >= 1 && testNumber <= 1000000 {
+                temp = 1
+            }
+        }
+    }
+    
+    temp = 0
+    // Section to take user input and put it into array
+    while temp <= 4 {
+        
+        // Declare a variable to temperarily store data from command line
+        print("Type a number to add to your queue for testing - (Number \(temp+1))")
+        if let inputTest = Int(readLine(strippingNewline: true)!) {
+            if inputTest >= 2 && inputTest <= 12 {
+                queue[temp] = inputTest
+                total = total + inputTest
+                temp += 1
+            }
+        }
+    }
+    for (index, value) in queue.enumerated() {
+        roll = Int(arc4random_uniform(10) + 2)
+        
+        for (index, value) in queue.enumerated() {
+            if roll == queue[index] {
+                if selected[index] == true {
+                    selected[index] = false
+                } else {
+                    selected[index] = true
+                }
+            }
+        }
+    }
+    
+    for (index, value) in queue.enumerated() {
+        if selected[index] == true {
+            hold = queue[index]
+            total = total - hold
+        }
+    }
+    
+    // TO DO: Add all total numbers after each test to array, then add them all up and divinde to find average. Display average as score after all the rounds
+    scores.append(total)
+    
+    print(total)
+    
+    print(total)
+    
 }
